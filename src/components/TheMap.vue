8aa0946690e71f507115c75fc1b112f783d29b20
machine-a-coudre <gellatine@gmail.com>
2024-02-05 15:44:04 +0100

    fix: use sdk mapContext

--- src/components/TheMap.vue
+++ src/components/TheMap.vue
@@ -6,7 +6,12 @@ import OSM from 'ol/source/OSM.js'
 import { onMounted, ref } from 'vue'
 
 const mapContext = {
-    layers: [],
+    layers: [
+      {
+        type: 'xyz',
+        url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+      }
+    ],
     view: {
         center: [0, 0] as Coordinate,
         zoom: 2
@@ -15,9 +20,9 @@ const mapContext = {
 const map = createMapFromContext(mapContext)
 const mapRef = ref<HTMLElement | undefined>(undefined)
 
-map.addLayer(new TileLayer({
-      source: new OSM(),
-    }))
+// map.addLayer(new TileLayer({
+//       source: new OSM(),
+//     }))
 
 onMounted(() => {
   map?.setTarget(mapRef.value);
