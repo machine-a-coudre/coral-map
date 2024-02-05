83ac5d690d5aefd66435c6d86ef6ef1bc668ec4c
machine-a-coudre <gellatine@gmail.com>
2024-02-05 15:31:59 +0100

    feat: include geospatial-sdk

---
+++ src/components/TheMap.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { Coordinate } from '@geospatial-sdk/core/lib/model'
+import { createMapFromContext } from '@geospatial-sdk/openlayers'
+import TileLayer from 'ol/layer/Tile'
+import OSM from 'ol/source/OSM.js'
+import { onMounted, ref } from 'vue'
+
+const mapContext = {
+    layers: [],
+    view: {
+        center: [0, 0] as Coordinate,
+        zoom: 2
+    }
+}
+const map = createMapFromContext(mapContext)
+const mapRef = ref<HTMLElement | undefined>(undefined)
+
+map.addLayer(new TileLayer({
+      source: new OSM(),
+    }))
+
+onMounted(() => {
+  map?.setTarget(mapRef.value);
+})
+</script>
+
+<template>
+    <div ref="mapRef" style="height: 600px; width: 600px">
+      <slot></slot>
+    </div>
+  </template>
